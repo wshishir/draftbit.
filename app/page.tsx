4552a62b136225fdf125aedd2539ceac0f679f56
Shishir <shishirtech506@gmail.com>
2026-06-14 00:40:25 +0530

    frontend: navbar comp done

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center ">
+    <div className="flex flex-col flex-1 h-screen bg-[#E8EDF2]">
+      <Navbar/>
       Hello is the blog site
     </div>
   );
---
+++ components/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div className="flex w-full items-center justify-between px-20 mt-5">
+      <div>
+        <Link href="/" className="font-semibold text-3xl cursor-pointer">
+          Draftbit
+        </Link>
+      </div>
+      <div className="flex items-center gap-10">
+        <a
+          href="https://github.com/wshishir/draftbit."
+          className="cursor-pointer"
+        >
+          <FaGithub className=" size-7" />
+        </a>
+        <Button className="px-7 py-4 text-md bg-[#2C3947] text-white font-light hover:bg-[#2C3947]/90 cursor-pointer">
+          Login
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
